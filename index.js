7077cef7f14baf574c8385c6c6e22c2a47cae91e
bdMithun <mithunmaxwell444@gmail.com>
2020-12-15 22:00:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+
+
+
+
+    //     <div class="skill " id="skill-id">
+
+    //     <div class="card">
+         
+    //         <div class="card-title">
+    //             <h2 class="text-center">SKILL</h2>
+
+    //         </div>
+    //         <div class="card-desc">
+    //             <div class="skill-container">
+
+    //                 <ul>
+    //                     <li class="html">HTML <span class="skill-parcent">90%</span></li>
+    //                     <li class="css">css <span class="skill-parcent">85%</span> </li>
+    //                     <li class="javascript">javascript <span class="skill-parcent">70%</span> </li>
+    //                     <li class="jquery">jquery <span class="skill-parcent">80%</span> </li>
+    //                     <li class="bootstrap">bootstrap <span class="skill-parcent">85%</span> </li>
+    //                     <li class="php">php <span class="skill-parcent">70%</span> </li>
+    //                     <li class="wordpress">wordpress <span class="skill-parcent">80%</span> </li>
+
+
+
+
+
+    //                 </ul>
+    //             </div>
+
+
+    //         </div>
+
+    //     </div>
+
+
+
+
+    // </div>
+
+    //    < div class = "col-lg-4 col-md-6 col-sm-12  " >
+
+    //        <
+    //        img src = "image/unify-1.png"
+    //    class = "work-image"
+    //    alt = "image" >
+    //        <
+    //        div class = "overlay " >
+    //        <
+    //        div class = "overlay-icon" >
+    //        <
+    //        i class = "fas fa-air-freshener fa-2x text-light " > < /i> <
+    //        /div> <
+    //        div class = "overlay-text" >
+    //        <
+    //        h2 > DESGIN COLLECTIVE < /h2> <
+    //        P > Branding / Presentation < /P> <
+    //        /div> <
+    //        /div>
+
+    //        <
+    //        /div>
